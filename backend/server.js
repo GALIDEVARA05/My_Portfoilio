fa01eddcc16ab8828680ec7204b04d6d65e86171
Galidevara Venkata Lokesh <galidevaravenkatalokesh05@gmail.com>
2025-07-27 14:31:19 +0530

    added for the render backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const Contact = require("./models/contact"); // adjust path if needed
+const Contact = require("./models/Contact"); // adjust path if needed
 
 dotenv.config();
 
